a9b4b994d8cf09c46945251a1df3c166ddc5a81c
Firoz Ansari <firozansar@gmail.com>
2021-03-22 05:42:27 +0000

    Add App class and Manifest change

---
+++ app/src/main/java/info/firozansari/redditreader/ui/App.kt
@@ -0,0 +1,11 @@
+package info.firozansari.redditreader.ui
+
+import android.app.Application
+
+class App : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+
+    }
+}
---
+++ app/src/main/java/info/firozansari/redditreader/util/Constant.kt
@@ -0,0 +1,6 @@
+package info.firozansari.redditreader.util
+
+object Constant {
+    const val BASE_URL = "https://www.reddit.com/"
+    const val GET_POSTS_ENDPOINT = "r/Android/hot.json"
+}
